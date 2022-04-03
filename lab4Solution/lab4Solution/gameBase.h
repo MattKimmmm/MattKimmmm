2c62f563f8d18a045ef3eea06fd5e094b068d70c
c.jinfeng <c.jinfeng@wustl.edu>
2022-04-03 15:37:05 -0500

    Make longestStrLen protected

--- lab4Solution/lab4Solution/gameBase.h
+++ lab4Solution/lab4Solution/gameBase.h
@@ -18,7 +18,7 @@ public:
 	int play();
 	int moves_num; //tracks total number of moves made by both players.
 
-	unsigned int longestStrLen;//tracks the longest display string length of any piece on the board
+	
 
 protected:
 	int boardWidth; //5
@@ -27,6 +27,8 @@ protected:
 	char winner;//saves game piece of the player who won the game.
 	std::vector<std::pair<int, int>> playerX; //vector of all moves by player X.
 	std::vector<std::pair<int, int>> playerO; //vector of all moves by player O.
+
+	unsigned int longestStrLen;//tracks the longest display string length of any piece on the board
 };
 
 class TicTacToe : public GameBase {
