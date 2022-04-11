2f93fd9a1559b1697dfaa742e7ae9723b4bd6ae6
c.jinfeng <c.jinfeng@wustl.edu>
2022-04-11 17:09:52 -0500

    Update userMessage() output

--- lab4Solution/lab4Solution/usageFunction.cpp
+++ lab4Solution/lab4Solution/usageFunction.cpp
@@ -6,6 +6,6 @@ using namespace std;
 
 //Function to print out user message when the command line argument is invalid.
 int GameBase::userMessage() {
-    cout << "Usage: lab3.exe TicTacToe" << endl;
+    cout << "Usage: lab3.exe TicTacToe OR lab3.exe Gomoku" << endl;
     return static_cast<int>(returnVal::incorrectInput);
 }
