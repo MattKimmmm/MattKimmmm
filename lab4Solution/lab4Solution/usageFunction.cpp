5e6787be324d85bced02ad01508c45ea278783c5
c.jinfeng <c.jinfeng@wustl.edu>
2022-04-12 17:56:31 -0500

    .usagemessage update

--- lab4Solution/lab4Solution/usageFunction.cpp
+++ lab4Solution/lab4Solution/usageFunction.cpp
@@ -7,7 +7,8 @@ using namespace std;
 //Function to print out user message when the command line argument is invalid.
 int GameBase::userMessage() {
     cout << "Usage: lab4Solution.exe TicTacToe OR lab4Solution.exe Gomoku" << endl;
-    cout << "If you would like to play customized Gomoku, Usage: lab4Solution.exe Gomoku [boardsize] [number of connected pieces to win]" << endl;
+    cout << "If you would like to play customized Gomoku," << endl;
+    cout << "Usage: lab4Solution.exe Gomoku[boardsize][number of connected pieces to win]" << endl;
     cout << "Example: lab4Solution.exe Gomoku 8 4" << endl;
     return static_cast<int>(returnVal::incorrectInput);
 }
